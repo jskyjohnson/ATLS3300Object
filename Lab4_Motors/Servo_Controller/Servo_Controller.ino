64490bb6ab732e99461195f44cdefadb493f6cbe
nullmage <jsky.johnson@gmail.com>
2019-02-27 08:36:05 -0700

    Adds Lab4 Servo_controller

--- Lab4_Motors/Servo_Controller/Servo_Controller.ino
+++ Lab4_Motors/Servo_Controller/Servo_Controller.ino
@@ -1,1 +1,20 @@
+#include <Stepper.h>
 
+const int stepsPerRevolution = 513; //According to the datasheet!
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  myStepper.setSpeed(25);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  myStepper.step(stepsPerRevolution);
+  delay(1000);
+
+  digitalWrite(13, LOW);
+  myStepper.step(-stepsPerRevolution);
+  delay(1000);
+}
