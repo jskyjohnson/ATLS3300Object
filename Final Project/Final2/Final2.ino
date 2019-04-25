9ac8706e426acac982d10c56f6751962b0218771
nullmage <jsky.johnson@gmail.com>
2019-04-25 14:56:34 -0600

    Adds final for Expo

--- Final Project/Final2/Final2.ino
+++ Final Project/Final2/Final2.ino
@@ -22,12 +22,26 @@ void loop() {
  */
 
 
+#include <NewPing.h>
 
+#define TRIGGER_PIN  10  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+unsigned int pingSpeed = 50;
+unsigned long pingTimer;
 
 #include <Servo.h>
+#include "ServoEaser.h"
 
 Servo left;
+ServoEaser LeftEaser;
 Servo right;
+ServoEaser RightEaser;
+
+int servoFrameMillis = 20; 
 
 #define PI 3.1415926535897932384626433832795
 #define HALF_PI 1.5707963267948966192313216916398
@@ -43,31 +57,81 @@ float ty;
 int leftVal;
 int rightVal;
 
+float a = 1;
+float b = 0.5;
+
+float c = 1.3;
+float d = 0.5;
+
+unsigned int changeSpeed = 30000;
+unsigned long changeTimer;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   left.attach(5);
   right.attach(6);
+
+  LeftEaser.begin( left, servoFrameMillis );
+  RightEaser.begin( right, servoFrameMillis );
   ti = 0;
   leftVal = 70;
   rightVal = 40;
-  right.write(rightVal);
+
+  //pingTimer = millis();
+
+  a = (float) random(5000, 240000) / 100000.0;
+  b = (float) random(5000, 240000) / 100000.0;
+  c = (float) random(5000, 240000) / 100000.0;
+  d = (float)random(5000, 240000) / 100000.0;
+  Serial.print(a);
+  Serial.print("\t");
+  Serial.print(b);
+  Serial.print("\t");
+  Serial.print(c);
+  Serial.print("\t");
+  Serial.print(d);
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  ti += 0.0001;
-
-  tx = 4*sin(11*PI*ti) + 6;
-  ty = 9*cos(10*PI*ti) + 14;
   
-  leftVal = LeftTurnTo(tx, ty);
-  rightVal = RightTurnTo(tx, ty);
-  left.write(leftVal);
-  right.write(rightVal);
+  LeftEaser.update();
+  RightEaser.update();
+  if( LeftEaser.hasArrived() || RightEaser.hasArrived() ) { 
+    ti += 2*PI / 600;
 
-  delay(15);
-  //Serial.println();
+    tx = 3*sin(a*PI*ti) + sin(c*PI*ti) + 5;
+    ty = 6*cos(b*PI*ti) + cos(d*PI*ti) + 14;
+     
+    rightVal = RightTurnTo(tx, ty);
+    leftVal = LeftTurnTo(tx, ty);
+    LeftEaser.easeTo( leftVal, 40 );
+    RightEaser.easeTo( rightVal, 40 );
+  }
+
+  if(millis() > changeTimer){
+    changeTimer += changeSpeed;
+    a = (float) random(5000, 240000) / 100000.0;
+    b = (float) random(5000, 240000) / 100000.0;
+    c = (float) random(5000, 240000) / 100000.0;
+    d = (float)random(5000, 240000) / 100000.0;
+    Serial.print(a);
+    Serial.print("\t");
+    Serial.print(b);
+    Serial.print("\t");
+    Serial.print(c);
+    Serial.print("\t");
+    Serial.print(d);
+    Serial.println();
+    ti = 0;
+  }
+
+//  if (millis() >= pingTimer) {   
+//    Serial.println("Ping should be goign out");
+//    pingTimer += pingSpeed;      
+//    sonar.ping_timer(echoCheck);
+//  }
 }
 
 float LeftTurnTo(float x, float y){
@@ -104,4 +168,12 @@ float RightTurnTo(float x, float y){
   
   float Servo_Angle = map(angle, 0.0, 90.0, 124.0, 21.0); //right vals 127.0, 23.0
   return Servo_Angle;
+}
+
+void echoCheck() {
+  if (sonar.check_timer()) {
+    Serial.print("Ping: ");
+    Serial.print(sonar.ping_result / US_ROUNDTRIP_CM);
+    Serial.println("cm");
+  }
 }
